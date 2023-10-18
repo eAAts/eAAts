ab4e0373a4118adba3269001c190f9653eaf3765
mg12 <volibear1001@gmail.com>
2023-10-18 19:56:47 +0900

    chore: Add comments

--- contracts/interfaces/IeAAts.sol
+++ contracts/interfaces/IeAAts.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+/**
+ * @title IeAAts
+ * @notice Interface for the eAAts contract
+ */
 interface IeAAts {
     // Enumerations
     enum DeliveryStatus {
@@ -47,15 +51,35 @@ interface IeAAts {
     event OrderDeliveryStarted(uint256 indexed orderId);
     event DeliveryCompleted(uint256 indexed orderId);
 
+    /**
+     * @notice Creates a new order
+     * @param _minParticipants Minimum number of participants required for the order
+     * @param _feeType Fee type of the order
+     * @return The ID of the created order
+     */
     function createOrder(
         uint256 _minParticipants,
         FeeType _feeType
     ) external returns (uint256);
 
+    /**
+     * @notice Allows a user to join an order
+     * @param _orderId ID of the order to join
+     * @param _amount Amount to contribute to the order
+     */
     function joinOrder(uint256 _orderId, uint256 _amount) external;
 
+    /**
+     * @notice Marks an order as completed (delivery done)
+     * @param _orderId ID of the order to complete
+     */
     function completeDelivery(uint256 _orderId) external;
 
+    /**
+     * @notice Fetches orders based on their delivery status
+     * @param _status Desired delivery status
+     * @return An array of orders matching the provided status
+     */
     function getOrdersByStatus(
         DeliveryStatus _status
     ) external view returns (OrderData[] memory);
