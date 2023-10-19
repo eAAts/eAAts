f356d1d499612a2974d8244e25ecec0e78e2ed52
solidity12 <volibear1001@gmail.com>
2023-10-19 19:03:31 +0900

    chore: Add hardhat network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,28 +1,32 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@openzeppelin/hardhat-upgrades");
-require('dotenv').config()
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  defaultNetwork: "hardhat",
   networks: {
     hardhat: {
+      forking: {
+        url: process.env.MUMBAI_ENDPOINT,
+      },
     },
     mumbai: {
       url: process.env.MUMBAI_ENDPOINT,
-      accounts: [process.env.PRIVATE_KEY]
-    }
+      accounts: [process.env.PRIVATE_KEY],
+    },
   },
   etherscan: {
-    apiKey: process.env.POLYGONSCAN_API_KEY
+    apiKey: process.env.POLYGONSCAN_API_KEY,
   },
   solidity: {
     version: "0.8.9",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
+        runs: 200,
+      },
+    },
   },
 };
